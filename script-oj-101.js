0487463e894e4e161c6f698642f3faa1315127d1
以牧 <fr148791@alibaba-inc.com>
2018-05-15 16:04:41 +0800

    add crawler & code

--- script-oj-101.js
+++ script-oj-101.js
@@ -1,3 +1,17 @@
 const extractStr = (str) => {
-  
+  let re = /(\:)(\w+)*(?=\.)/g;
+  // let result = re.exec(str);
+  let arr = str.match(re);
+  if (arr === null) {
+    return [];
+  }
+  arr = arr.map((value, index, array) => {
+    return value.substr(1);
+  });
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+  return arr || [];
 }
+
+extractStr(':.:.::lYzsKc.OBxwcQQnbrJK');
--- script-oj-102.js
+++ script-oj-102.js
@@ -1,3 +1,50 @@
-const fibonacci = (n) => {
-  
+function cacheFunc(f) {
+  let cache = {};
+  return function () {
+    let key = JSON.stringify(arguments);
+    ret = cache[key]
+    return ret || (cache[key] = f(...arguments));
+  }
+}
+
+/*
+F is a functional, i.e. a function that takes a function. Y will take that functional 
+and produce a fixed point for it
+*/
+function Y(F, cache) {
+  cache = cache || {};
+  return function (arg) {
+    var answer;
+    if (cache[arg]) {
+      return cache[arg]; // Answer in cache.
+    }
+    answer = (F(function (n) {
+      return (Y(F, cache))(n);
+    }))(arg);
+    cache[arg] = answer; // Cache the answer.
+    return answer;
+  };
+}
+
+function mkFib(g) {
+  return function (n) {
+    return n < 2 ? n : g(n - 1) + g(n - 2);
+  }
+}
+
+const fibonacci = Y(mkFib);
+
+// will TLE !
+// const fibonacci = (n) => {
+//   return Y(mkFib)(n);
+// };
+
+// Wrong
+// const fibonacci = cacheFunc(mkFib);
+
+for (let i = 0; i < 20; i++) {
+  console.log(i + ':::');
+  console.time("test" + i);
+  console.log(fibonacci(1000));
+  console.timeEnd("test" + i);
 }
